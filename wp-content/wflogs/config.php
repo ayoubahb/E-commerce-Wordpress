9c662207a669e0128e39856e752aa2621453ba2e
ayoubahb <ayoubahabbane@gmail.com>
2023-03-15 09:45:59 +0100

    first commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1678963921;s:7:"authKey";s:64:"P5^PPQT0JWf OXqjS:KU::!w|.wQoRGoC7@BY^32`I9/)1?,WlKp>EZUu(TX4YpR";s:7:"version";s:5:"1.0.5";s:13:"attackDataKey";i:3131;}
